6723c85b683bb5290169247eded1f3f1a320512b
Benji250 <123209910+Benji250@users.noreply.github.com>
2023-05-31 20:02:15 -0700

    sending index and show data

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ res.send('Welcome to an Awesome App about Breads!')
 })
 
 //Breads
-const breadsController = require('./controllers/breads_controllers.js')
+const breadsController = require('./controllers/breads_controller.js')
 app.use('/breads', breadsController)
 
 //LIsten
